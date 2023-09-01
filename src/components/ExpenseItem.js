aecc6879b1dba9c5b87e43c159ea40ad31585524
Ashish7520 <ashishnandwana2@gmail.com>
2023-09-01 22:43:50 +0530

    react starts

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem() {
+    return (
+        <p>
+    Expense Items <br></br>
+    Food Rs 10 <br></br>
+    Petrol Rs 100 <br></br>
+    Movies Rs 200
+        </p>
+    )
+}
+
+export default ExpenseItem

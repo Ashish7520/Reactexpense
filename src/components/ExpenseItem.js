af6c2a612ad9e294ca3ab7409ec785dfcada6004
Ashish7520 <ashishnandwana2@gmail.com>
2023-09-01 23:34:00 +0530

    Styling and using JS in JSX

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,21 @@
+import './ExpenseItem.css'
+
 function ExpenseItem() {
-    return (
-        <p>
-    Expense Items <br></br>
-    Food Rs 10 <br></br>
-    Petrol Rs 100 <br></br>
-    Movies Rs 200
-        </p>
-    )
+ 
+    //use dynamic data
+    const expenseDate = new Date(2023, 8, 1);
+    const expenseTitle = 'Car insurance';
+    const expenseAmount = 49.99
+    const LocationOfExpenditure = 'Garage'
+
+    return  <div className='expense-item'> 
+        <div>{expenseDate.toISOString()}</div>
+        <div className='expense-item__description '> 
+            <h2>{expenseTitle}</h2>
+            <div className='expense-item__price'>${expenseAmount}</div>
+            <div className='expense-item__location'>{LocationOfExpenditure}</div>
+        </div>
+    </div>
 }
 
 export default ExpenseItem

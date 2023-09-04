a8d06bebea38b915891f7e4a5cefb820cfea7ab0
Ashish7520 <ashishnandwana2@gmail.com>
2023-09-04 20:04:45 +0530

    splitting components
    Splitting the components for better code readability

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,11 @@
+import ExpenseDate from './ExpenseDate';
+import ExpenseDetails from './ExpenseDetails';
 import './ExpenseItem.css'
 
 function ExpenseItem(props) {
     return  <div className='expense-item'> 
-        <div>{props.expenseDate.toISOString()}</div>
-        <div className='expense-item__description '> 
-            <h2>{props.title}</h2>
-            <div className='expense-item__price'>${props.amount}</div>
-            <div className='expense-item__location'>{props.Location}</div>
-        </div>
+        <ExpenseDate expenseDate={props.expenseDate}></ExpenseDate>
+        <ExpenseDetails title={props.title} amount={props.amount} Location={props.Location}></ExpenseDetails>
     </div>
 }
 

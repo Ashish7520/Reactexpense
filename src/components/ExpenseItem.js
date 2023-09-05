ba127d7848e20fff21689f3db375ec661044ec8f
Ashish7520 <ashishnandwana2@gmail.com>
2023-09-05 21:33:00 +0530

    adding dummy event listener

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,9 +3,19 @@ import ExpenseDetails from './ExpenseDetails';
 import './ExpenseItem.css'
 
 function ExpenseItem(props) {
+    const clickHandler = ()=>{
+        console.log("clicked")
+    }
+
+    const deleteHandler = ()=>{
+        console.log("deleted")
+    }
+
     return  <div className='expense-item'> 
         <ExpenseDate expenseDate={props.expenseDate}></ExpenseDate>
         <ExpenseDetails title={props.title} amount={props.amount} Location={props.Location}></ExpenseDetails>
+        <button onClick={clickHandler}>Change Title</button>
+        <button onClick={deleteHandler}>Delete</button>
     </div>
 }
 
